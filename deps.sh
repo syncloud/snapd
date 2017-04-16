51b475492f73a19d9e2156b80e01627d0ee402b5
Boris Rybalkin <ribalkin@gmail.com>
2017-04-16 22:56:02 +0100

    gnupg2 dep

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools git
+sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools git gnupg2
 rm -rf /usr/bin/go
 ln -s /usr/lib/go-1.6/bin/go /usr/bin/go
 ln -s /usr/lib/go-1.6/bin/gofmt /usr/bin/gofmt
