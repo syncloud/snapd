115e0890c3c47bf03b51e76d7973a9ffeca70432
Boris Rybalkin <ribalkin@gmail.com>
2017-04-17 17:00:48 +0100

    fix jessie

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 if [[ $(lsb_release -sc) == "jessie" ]]; then
-    cat "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
+    echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 fi
 
 sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools git gnupg2 gettext
