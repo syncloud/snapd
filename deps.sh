49bf96249f9e892f52c2ba64f6a0ce178694689b
Boris Rybalkin <ribalkin@gmail.com>
2017-04-17 19:18:42 +0100

    fix

--- deps.sh
+++ deps.sh
@@ -3,8 +3,8 @@
 if [[ $(lsb_release -sc) == "jessie" ]]; then
     echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 fi
-
-sudo apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools git gnupg2 gettext
+apt-get update
+apt-get install -y libusb-dev golang-1.6 build-essential autoconf libglib2.0-dev libseccomp-dev libapparmor-dev python-docutils libudev-dev squashfs-tools git gnupg2 gettext
 rm -rf /usr/bin/go
 ln -s /usr/lib/go-1.6/bin/go /usr/bin/go
 rm -rf /usr/bin/gofmt
