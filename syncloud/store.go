bf159cb847b963d8e6880df4bc6983d5bcc70f88
Boris Rybalkin <ribalkin@gmail.com>
2018-05-12 13:51:12 +0100

    fix sign key sha

--- syncloud/store.go
+++ syncloud/store.go
@@ -24,7 +24,7 @@ import (
 	"bytes"
 	"crypto"
 	"encoding/json"
-	"encoding/base64"
+	//"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
@@ -1079,15 +1079,15 @@ func (s *Store) Assertion(assertType *asserts.AssertionType, primaryKey []string
 	logger.Noticef("assert key: %s", strings.Join(primaryKey, "/"))
 
 	blobSHA3_384 := "QlqR0uAWEAWF5Nwnzj5kqmmwFslYPu1IL16MKtLKhwhv0kpBv5wKZ_axf_nf_2cL"
-	hashDigest, err := base64.RawURLEncoding.DecodeString(blobSHA3_384)
-	if err != nil {
-		return nil, err
-	}
+	//hashDigest, err := base64.RawURLEncoding.DecodeString(blobSHA3_384)
+	//if err != nil {
+	//	return nil, err
+	//}
 
-	digest, err := asserts.EncodeDigest(crypto.SHA3_384, hashDigest)
-	if err != nil {
-		return nil, err
-	}
+	//digest, err := asserts.EncodeDigest(crypto.SHA3_384, hashDigest)
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	publicKeyEnc, err := asserts.EncodePublicKey(privkey.PublicKey())
 	if err != nil {
@@ -1115,9 +1115,9 @@ func (s *Store) Assertion(assertType *asserts.AssertionType, primaryKey []string
 		"account-id: syncloud\n" +
 		"display-name: syncloud\n" +
 		"revision: 1\n" +
-		"sign-key-sha3-384: " + digest + "\n" +
-		"sha3-384: " + digest + "\n" +
-		"snap-sha3-384: " + digest + "\n" +
+		"sign-key-sha3-384: " + blobSHA3_384 + "\n" +
+		"sha3-384: " + blobSHA3_384 + "\n" +
+		"snap-sha3-384: " + blobSHA3_384 + "\n" +
 		"public-key-sha3-384: " + privkey.PublicKey().ID() + "\n" +
 		"timestamp: " + time.Now().Format(time.RFC3339) + "\n" +
 		"since: " + time.Now().Format(time.RFC3339) + "\n" +
