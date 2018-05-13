37a71d4ae8c1f56f13b0a86bb139e45323a5aeff
Boris Rybalkin <ribalkin@gmail.com>
2018-05-13 08:42:53 +0100

    expose signContent

--- syncloud/store.go
+++ syncloud/store.go
@@ -1127,7 +1127,7 @@ func (s *Store) Assertion(assertType *asserts.AssertionType, primaryKey []string
 		body +
 		"\n\n"
 		
-		signature, err := asserts.signContent(content, privkey)
+		signature, err := asserts.SignContent(content, privkey)
 		
 		if err != nil {
 		return nil, err
