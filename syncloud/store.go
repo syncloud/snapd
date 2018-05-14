ca85c6f2f895f9c9c2d3dae6d63ab3f83007ebf3
Boris Rybalkin <ribalkin@gmail.com>
2018-05-14 07:58:18 +0100

    fix syntax

--- syncloud/store.go
+++ syncloud/store.go
@@ -1250,7 +1250,7 @@ func buyOptionError(message string) (*BuyResult, error) {
 func (s *Store) LookupRefresh(installed *store.RefreshCandidate, user *auth.UserState) (*snap.Info, error) {
 	logger.Noticef("LookupRefresh")
  channel := installed.Channel
- snapName, snapVersion := deconstructSnapId(installed.SnapID))
+ snapName, snapVersion := deconstructSnapId(installed.SnapID)
 	versions, err := s.downloadVersions(channel)
 
  	if err != nil {
