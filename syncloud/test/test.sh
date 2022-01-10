7603c8b0e40c6e3ac0a5bc9f274e224ebd14315c
Boris Rybalkin <ribalkin@gmail.com>
2022-01-10 20:37:31 +0000

    fix ssh

--- syncloud/test/test.sh
+++ syncloud/test/test.sh
@@ -9,7 +9,7 @@ fi
 
 VERSION=$1
 SCP="sshpass -p syncloud scp -o StrictHostKeyChecking=no"
-SSH="sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@"
+SSH="sshpass -p syncloud ssh -o StrictHostKeyChecking=no"
 ARCH=$(dpkg --print-architecture)
 LOG_DIR=${DIR}/../../log
 
@@ -41,27 +41,27 @@ wait_for_host store
 
 mkdir -p $LOG_DIR
 
-$SSH store apt update && apt install -y nginx tree
-$SSH store mkdir -p /var/www/html/releases/master
-$SSH store mkdir -p /var/www/html/apps
+$SSH root@store apt update && apt install -y nginx tree
+$SSH root@store mkdir -p /var/www/html/releases/master
+$SSH root@store mkdir -p /var/www/html/apps
 $SCP ${DIR}/../../syncloud-release-$ARCH root@store:/syncloud-release
 $SCP ${DIR}/../test/testapp1/testapp1_1_$ARCH.snap root@store:/
 $SCP ${DIR}/../test/testapp2/testapp2_1_$ARCH.snap root@store:/
-$SSH store /syncloud-release publish -f /testapp1_1_$ARCH.snap -b master -t /var/www/html
-$SSH store /syncloud-release publish -f /testapp2_1_$ARCH.snap -b master -t /var/www/html
+$SSH root@store /syncloud-release publish -f /testapp1_1_$ARCH.snap -b master -t /var/www/html
+$SSH root@store /syncloud-release publish -f /testapp2_1_$ARCH.snap -b master -t /var/www/html
 $SCP ${DIR}/index-v2 root@store:/var/www/html/releases/master
-$SSH store tree /var/www/html > $LOG_DIR/store.tree.log
-$SSH store systemctl status nginx > $LOG_DIR/nginx.status.log
+$SSH root@store tree /var/www/html > $LOG_DIR/store.tree.log
+$SSH root@store systemctl status nginx > $LOG_DIR/nginx.status.log
 
 $SCP ${DIR}/install-snapd.sh root@device:/installer.sh
 $SCP ${DIR}/../../snapd-${VERSION}-*.tar.gz root@device:/
 
-$SSH device /installer.sh ${VERSION}
+$SSH root@device /installer.sh ${VERSION}
 
 #code=0
 set +e
-$SSH device snap install testapp1 --channel=master
-$SSH device snap install testapp2 --channel=master
+$SSH root@device snap install testapp1 --channel=master
+$SSH root@device snap install testapp2 --channel=master
 #$SSH snap install files
 code=$?
 #$SSH snap refresh files
@@ -69,7 +69,7 @@ code=$?
 set -e
 
 #mkdir -p log
-$SSH device snap changes > $LOG_DIR/snap.changes.log || true
-$SSH device journalctl > $LOG_DIR/journalctl.device.log
+$SSH root@device snap changes > $LOG_DIR/snap.changes.log || true
+$SSH root@device journalctl > $LOG_DIR/journalctl.device.log
 $SSH store journalctl > $LOG_DIR/journalctl.store.log
 exit $code
