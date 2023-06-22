e3ef98463240b8af31cd5069d7824f595c0ff13b
Boris Rybalkin <ribalkin@gmail.com>
2023-06-22 09:57:57 +0100

    fix test

--- syncloud/test/deploy_test.go
+++ syncloud/test/deploy_test.go
@@ -80,7 +80,7 @@ func TestApps(t *testing.T) {
 	assert.NoError(t, err, output)
 
 	client := &http.Client{}
-	_, err = client.Post("http://apps.syncloud.it:8080//v2/snaps/info/testapp1?architecture=arm64&fields=architectures", "", nil)
+	_, err = client.Post("http://device:8080/v2/snaps/info/testapp1?architecture=arm64&fields=architectures", "", nil)
 	assert.NoError(t, err, output)
 }
 
