f6e7065bd6d329b4f4ef15266b5f5d64dcb53b6c
Boris Rybalkin <ribalkin@gmail.com>
2025-02-17 23:07:51 +0000

    4.6.1

--- build-squashfs.sh
+++ build-squashfs.sh
@@ -5,14 +5,15 @@ apt update
 apt install -y wget xfslibs-dev libzstd-dev liblz-dev liblz4-dev liblzo2-dev zlib1g-dev liblzma-dev build-essential
 NAME=snapd
 BUILD_DIR=${DIR}/build/${NAME}
+VERSION=4.6.1
 
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 mkdir ${BUILD_DIR}/bin
 
-wget https://github.com/plougher/squashfs-tools/archive/refs/tags/4.4.tar.gz
-tar xf 4.4.tar.gz
-cd squashfs-tools-4.4/squashfs-tools
+wget https://github.com/plougher/squashfs-tools/archive/refs/tags/$VERSION.tar.gz
+tar xf $VERSION.tar.gz
+cd squashfs-tools-$VERSION/squashfs-tools
 sed -i 's/#XZ_SUPPORT.*/XZ_SUPPORT=1/g' Makefile
 sed -i 's/#LZO_SUPPORT.*/LZO_SUPPORT=1/g' Makefile
 sed -i 's/#LZ4_SUPPORT.*/LZ4_SUPPORT=1/g' Makefile
