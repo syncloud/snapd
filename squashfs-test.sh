2a462a3955784517761a1387c951a7478c1c1e66
Boris Rybalkin <ribalkin@gmail.com>
2025-02-26 22:20:33 +0000

    fix test

--- squashfs-test.sh
+++ squashfs-test.sh
@@ -4,5 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
 ./build/snapd/bin/unsquashfs -ll .syncloud/test/testapp1_1_*.snap
-./build/snapd/bin/unsquashfs --help
-./build/snapd/bin/squashfs --help
+./build/snapd/bin/unsquashfs -h || grep
+./build/snapd/bin/unsquashfs -v
+./build/snapd/bin/squashfs -h || true
+./build/snapd/bin/squashfs -v
