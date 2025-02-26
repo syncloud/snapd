73a69d66cc39d9ff0418a6ed06b6fef7c3086203
Boris Rybalkin <ribalkin@gmail.com>
2025-02-26 22:57:49 +0000

    fix mk

--- squashfs-test.sh
+++ squashfs-test.sh
@@ -5,4 +5,4 @@ cd $DIR
 
 ./build/snapd/bin/unsquashfs -ll .syncloud/test/testapp1_1_*.snap
 ./build/snapd/bin/unsquashfs -h | grep xz
-./build/snapd/bin/squashfs -h | gtep xz
+./build/snapd/bin/mksquashfs -h | grep xz
